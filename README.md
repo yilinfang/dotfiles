149339e627bf40d0da4b27dd339bcc5caa4ae382
Yilin Fang <qzfyl98@outlook.com>
2025-03-13 01:46:15 -0400

    Add tmux

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 Personal dotfiles including:
 
-- git
-- vim
-- neovim
-- zellij
 - fish
-- starship
-- Ghostty
+- ghostty
+- git
 - kitty
+- neovim
+- starship
+- tmux
+- vim
 - wezterm
 
 I code with vim and VSCode, the Neovim configuration is mainly for VSCode-neovim plugin.
@@ -21,6 +21,7 @@ git clone https://github.com/yilinfang/dotfiles.git
 cd dotfiles
 git fetch --all
 git checkout vsc
+git clone https://github.com/yilinfang/tmux.git
 chmod +x intall.sh
 ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -40,6 +40,10 @@ create_link "$DOTFILES_DIR/git/.gitconfig" "$HOME/.gitconfig"
 # Vim configuration
 create_link "$DOTFILES_DIR/vim/.vimrc" "$HOME/.vimrc"
 
+# tmux configuration
+create_link "$DOTFILES_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+create_link "$DOTFILES_DIR/tmux/.tmux.conf.local" "$HOME/.tmux.conf.local"
+
 # Ghostty configuration
 create_link "$DOTFILES_DIR/ghostty" "$HOME/.config/ghostty"
 
@@ -57,6 +61,3 @@ create_link "$DOTFILES_DIR/starship/starship.toml" "$HOME/.config/starship.toml"
 
 # Neovim configuration
 create_link "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
-
-# Zellij configuration
-create_link "$DOTFILES_DIR/zellij" "$HOME/.config/zellij"
