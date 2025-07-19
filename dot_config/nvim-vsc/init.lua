761457c8ca3d47d5204d3834651c66c7af3e9940
Yilin Fang <qzfyl98@outlook.com>
2025-07-19 13:53:36 -0400

    feat(nvim-vsc): Update nvim-vsc configurations

--- dot_config/nvim-vsc/init.lua
+++
@@ -1,9 +0,0 @@
--- NeoVim Configuration
--- Copyright (C) 2025 Yilin Fang
-
--- Set leader and localleader keys
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Load the VSCode module
-require "code"
