d2aa3c218a219c5123261f6e4b34963ac242047f
Yilin Fang <qzfyl98@outlook.com>
2025-07-26 15:56:07 -0400

    feat(nvim): Add keymap for picking registry

--- dot_config/nvim/lua/plugins/config/mini/pick.lua
+++ dot_config/nvim/lua/plugins/config/mini/pick.lua
@@ -14,7 +14,7 @@ if pick_ok then
   vim.keymap.set("n", "<leader>st", "<cmd>Pick treesitter<cr>", { desc = "[S]earch [T]reesitter Nodes" })
   vim.keymap.set("n", "<leader>s.", "<cmd>Pick oldfiles<cr>", { desc = "[S]earch Old Files (['.'] for repeat)" })
   vim.keymap.set("n", "<leader>sm", "<cmd>Pick marks<cr>", { desc = "[S]earch [M]arks" })
-  vim.keymap.set("n", "<leader>sH", "<cmd>Pick hipatterns<cr>", { desc = "[S]earch [H]ipatterns" })
+  vim.keymap.set("n", "<leader>sp", "<cmd>Pick hipatterns<cr>", { desc = "[S]earch Hi[p]atterns" })
   -- [[ LSP pickers ]]
   -- NOTE: Only available when LSP is attached
   --  It will override some of the default LSP keymaps
@@ -63,25 +63,39 @@ if pick_ok then
   -- Map <leader>sj to search jumplist
   vim.keymap.set("n", "<leader>sj", "<cmd>Pick list scope='jump'<cr>", { desc = "[S]earch [J]umplist" })
 
+  -- Customized pickers for file and grep
+  -- HACK: By changing the RIPGREP_CONFIG_PATH to use a custom configuration
+  -- From: https://github.com/echasnovski/mini.nvim/issues/1859#issuecomment-2979332899
+
   -- Map <leader>sf to search files
   pick.registry.customized_files = function()
-    local cache_rg_config = vim.uv.os_getenv "RIPGREP_CONFIG_PATH"
+    local cache_rg_config = vim.uv.os_getenv "RIPGREP_CONFIG_PATH" or ""
     -- Temporarily set the RIGPREP_CONFIG_PATH to ~/.config/nvim/.rg/files
     vim.uv.os_setenv("RIPGREP_CONFIG_PATH", vim.fn.stdpath "config" .. "/.rg/files")
     pick.builtin.files { tool = "rg" }
     -- Restore the original RIPGREP_CONFIG_PATH
-    vim.uv.os_setenv("RIPGREP_CONFIG_PATH", cache_rg_config)
+    if cache_rg_config == "" then
+      -- If RIPGREP_CONFIG_PATH was not set, remove it to avoid side effects
+      vim.uv.os_unsetenv "RIPGREP_CONFIG_PATH"
+    else
+      vim.uv.os_setenv("RIPGREP_CONFIG_PATH", cache_rg_config)
+    end
   end
   vim.keymap.set("n", "<leader>sf", "<cmd>Pick customized_files<cr>", { desc = "[S]earch [F]iles" })
 
   -- Map <leader>sF to search files (including files ignored by git)
   pick.registry.customized_files_all = function()
-    local cache_rg_config = vim.uv.os_getenv "RIPGREP_CONFIG_PATH"
+    local cache_rg_config = vim.uv.os_getenv "RIPGREP_CONFIG_PATH" or ""
     -- Temporarily set the RIGPREP_CONFIG_PATH to ~/.config/nvim/.rg/files_all
     vim.uv.os_setenv("RIPGREP_CONFIG_PATH", vim.fn.stdpath "config" .. "/.rg/files_all")
     pick.builtin.files { tool = "rg" }
     -- Restore the original RIPGREP_CONFIG_PATH
-    vim.uv.os_setenv("RIPGREP_CONFIG_PATH", cache_rg_config)
+    if cache_rg_config == "" then
+      -- If RIPGREP_CONFIG_PATH was not set, remove it to avoid side effects
+      vim.uv.os_unsetenv "RIPGREP_CONFIG_PATH"
+    else
+      vim.uv.os_setenv("RIPGREP_CONFIG_PATH", cache_rg_config)
+    end
   end
   vim.keymap.set(
     "n",
@@ -92,23 +106,33 @@ if pick_ok then
 
   -- Map <leader>sg to search grep
   pick.registry.customized_grep_live = function()
-    local cache_rg_config = vim.uv.os_getenv "RIPGREP_CONFIG_PATH"
+    local cache_rg_config = vim.uv.os_getenv "RIPGREP_CONFIG_PATH" or ""
     -- Temporarily set the RIGPREP_CONFIG_PATH to ~/.config/nvim/.rg/grep
     vim.uv.os_setenv("RIPGREP_CONFIG_PATH", vim.fn.stdpath "config" .. "/.rg/grep")
     pick.builtin.grep_live { tool = "rg" }
     -- Restore the original RIPGREP_CONFIG_PATH
-    vim.uv.os_setenv("RIPGREP_CONFIG_PATH", cache_rg_config)
+    if cache_rg_config == "" then
+      -- If RIPGREP_CONFIG_PATH was not set, remove it to avoid side effects
+      vim.uv.os_unsetenv "RIPGREP_CONFIG_PATH"
+    else
+      vim.uv.os_setenv("RIPGREP_CONFIG_PATH", cache_rg_config)
+    end
   end
   vim.keymap.set("n", "<leader>sg", "<cmd>Pick customized_grep_live<cr>", { desc = "[S]earch by [G]rep" })
 
   -- Map <leader>sG to search grep (including files ignored by git)
   pick.registry.customized_grep_live_all = function()
-    local cache_rg_config = vim.uv.os_getenv "RIPGREP_CONFIG_PATH"
+    local cache_rg_config = vim.uv.os_getenv "RIPGREP_CONFIG_PATH" or ""
     -- Temporarily set the RIGPREP_CONFIG_PATH to ~/.config/nvim/.rg/grep_all
     vim.uv.os_setenv("RIPGREP_CONFIG_PATH", vim.fn.stdpath "config" .. "/.rg/grep_all")
     pick.builtin.grep_live { tool = "rg" }
     -- Restore the original RIPGREP_CONFIG_PATH
-    vim.uv.os_setenv("RIPGREP_CONFIG_PATH", cache_rg_config)
+    if cache_rg_config == "" then
+      -- If RIPGREP_CONFIG_PATH was not set, remove it to avoid side effects
+      vim.uv.os_unsetenv "RIPGREP_CONFIG_PATH"
+    else
+      vim.uv.os_setenv("RIPGREP_CONFIG_PATH", cache_rg_config)
+    end
   end
   vim.keymap.set(
     "n",
@@ -116,6 +140,18 @@ if pick_ok then
     "<cmd>Pick customized_grep_live_all<cr>",
     { desc = "[S]earch by [G]rep (including ignored files)" }
   )
+
+  -- Map <leader>ss to pick registry
+  -- From: https://github.com/echasnovski/mini.pick/blob/c8f4ff0251ccb8c6a993ee0dee4e06d9c21a4b99/doc/mini-pick.txt#L1218
+  pick.registry.registry = function()
+    local items = vim.tbl_keys(MiniPick.registry)
+    table.sort(items)
+    local source = { items = items, name = "Registry", choose = function() end }
+    local chosen_picker_name = MiniPick.start { source = source }
+    if chosen_picker_name == nil then return end
+    return MiniPick.registry[chosen_picker_name]()
+  end
+  vim.keymap.set("n", "<leader>ss", "<cmd>Pick registry<cr>", { desc = "[S]earch Regi[s]try" })
 else
   vim.notify("mini.pick not found", vim.log.levels.WARN)
 end
