8195dd44140c91b1a83472b60171200a1418c393
Yilin Fang <qzfyl98@outlook.com>
2025-08-08 22:18:04 -0400

    fix(p10k): Update color used in pure style

--- home/dot_p10k.zsh
+++ home/dot_p10k.zsh
@@ -38,7 +38,7 @@
   [[ $ZSH_VERSION == (5.<1->*|<6->.*) ]] || return
 
   # Prompt colors.
-  local grey='242'
+  local grey='8' # Use '8' instead of '242' to improve readability
   local red='1'
   local yellow='3'
   local blue='4'
