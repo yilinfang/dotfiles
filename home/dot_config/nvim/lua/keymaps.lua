fcf2c72dce42779a34d7dcbf43f748d8f892d941
Yilin Fang <qzfyl98@outlook.com>
2025-09-07 14:05:30 -0400

    feat(nvim): Add keymaps for switching buffers and fix j/k in word wrap

--- home/dot_config/nvim/lua/keymaps.lua
+++ home/dot_config/nvim/lua/keymaps.lua
@@ -8,8 +8,8 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Go to previous buffer" })
-vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Go to next buffer" })
+vim.keymap.set("n", "gt", "<cmd>bnext<cr>", { desc = "Go to next buffer" })
+vim.keymap.set("n", "gT", "<cmd>bprevious<cr>", { desc = "Go to previous buffer" })
 vim.keymap.set(
   "n",
   "<leader>f",
@@ -28,3 +28,8 @@ vim.keymap.set("n", "<leader>tw", function()
   local status = vim.opt_local.wrap:get() and "ON" or "OFF"
   print("Wrap: " .. status)
 end, { desc = "[T]oggle [W]rap" })
+
+-- HACK: Better up/down
+--  From LazyVim: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
