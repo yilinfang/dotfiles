b98f812cc89a8fc3afdd26505acbb00ba1f05548
Yilin Fang <qzfyl98@outlook.com>
2025-09-12 16:01:56 -0400

    feat(nvim): Disable format on save of conform.nvim

--- home/dot_config/nvim/lua/plugins/config/conform.lua
+++ home/dot_config/nvim/lua/plugins/config/conform.lua
@@ -30,24 +30,32 @@ end
 conform.setup {
   formatters_by_ft = formatters_by_ft,
   notify_on_error = false,
-  format_on_save = function(bufnr)
-    -- Disable `format_on_save lsp_fallback` for languages that don't
-    -- have a well standardized coding style. You can add additional
-    -- languages here or re-enable it for the disabled ones.
-    local disable_filetypes = { c = true, cpp = true }
-    if disable_filetypes[vim.bo[bufnr].filetype] then
-      return nil
-    else
-      return {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      }
-    end
-  end,
+  -- NOTE: Disable automatic formatting on save.
+  format_on_save = nil,
+  -- format_on_save = function(bufnr)
+  --   -- Disable `format_on_save lsp_fallback` for languages that don't
+  --   -- have a well standardized coding style. You can add additional
+  --   -- languages here or re-enable it for the disabled ones.
+  --   local disable_filetypes = { c = true, cpp = true }
+  --   if disable_filetypes[vim.bo[bufnr].filetype] then
+  --     return nil
+  --   else
+  --     return {
+  --       timeout_ms = 500,
+  --       lsp_format = "fallback",
+  --     }
+  --   end
+  -- end,
 }
 vim.keymap.set(
   "n",
   "grf",
   function() conform.format { async = true, lsp_format = "fallback" } end,
   { desc = "Conform: [F]ormat Buffer" }
+)
+vim.keymap.set(
+  "v",
+  "grf",
+  function() conform.format { async = true, lsp_format = "fallback" } end,
+  { desc = "Conform: [F]ormat Selection" }
 )
