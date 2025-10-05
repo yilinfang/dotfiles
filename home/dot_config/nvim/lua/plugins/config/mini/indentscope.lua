15dbd303dd3dd8bb4ceafa64cf704136911bba46
Yilin Fang <qzfyl98@outlook.com>
2025-10-05 09:48:24 -0400

    revert(nvim): Bring indentscope back

--- home/dot_config/nvim/lua/plugins/config/mini/indentscope.lua
+++ home/dot_config/nvim/lua/plugins/config/mini/indentscope.lua
@@ -1,14 +1,12 @@
 -- lua/plugins/config/mini/indentscope.lua
 -- Configuration for `mini.indentscope`
 
--- NOTE: Do not show indent guides
--- We only use textobject feature
-vim.g.miniindentscope_disable = true
-
 local indentscope = require "mini.indentscope"
 local opts = {
   draw = {
+    delay = 0,
     animation = indentscope.gen_animation.none(),
   },
+  options = { try_as_border = true },
 }
 indentscope.setup(opts)
