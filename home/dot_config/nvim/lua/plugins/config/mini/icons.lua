a8be4af515ae86c62d3dc4feb153f28cd8f39752
Yilin Fang <qzfyl98@outlook.com>
2025-10-13 11:03:32 -0400

    feat(nvim): Enable LSP kind icons of mini.icons

--- home/dot_config/nvim/lua/plugins/config/mini/icons.lua
+++ home/dot_config/nvim/lua/plugins/config/mini/icons.lua
@@ -4,4 +4,9 @@
 local icons = require "mini.icons"
 local opts = {}
 icons.setup(opts)
+
+-- Mock 'nvim-tree/nvim-web-devicons' for plugins without 'mini.icons' support
 icons.mock_nvim_web_devicons()
+
+-- Add LSP kind icons
+icons.tweak_lsp_kind()
