adab51c122ec7f396100f509dd093d016fa92203
Yilin Fang <qzfyl98@outlook.com>
2025-10-15 02:04:21 -0400

    docs(nvim): Add some description

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,6 +1,13 @@
--- [[
--- Yilin Fang's Personal Neovim Configuration
--- Copyright (c) 2025 Yilin Fang
---
--- TODO: Add more description here
--- ]]
+--[[
+
+Yilin Fang's Personal Neovim Configuration
+A miminal setup based on `mini.nvim` (https://github.com/nvim-mini/mini.nvim)
+Copyright (c) 2025 Yilin Fang
+
+Very thanks to:
+  - `AstroNvim` (https://astronvim.com/)
+  - `Kickstart.nvim` (https://github.com/nvim-lua/kickstart.nvim)
+  - `LazyVim` (https://www.lazyvim.org/)
+  - `MiniMax` (https://github.com/nvim-mini/MiniMax)
+
+--]]
