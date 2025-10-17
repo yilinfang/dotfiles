b5e46996bc112cc71e13dd49608c4c03f2836b14
Yilin Fang <qzfyl98@outlook.com>
2025-10-16 21:12:44 -0400

    feat(*): Theme tokyonight-night

---
+++ home/dot_config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,30 @@
+-- lua/plugins/tokyonight.lua
+-- Configuration for theme `tokyonight.nvim`
+
+local tokyonight = require('tokyonight')
+local opts = {
+  style = 'night',
+  plugins = {
+    all = true, -- NOTE: Enable all plugins
+    auto = false, -- NOTE: We are not using `lazy.nvim`
+  },
+}
+tokyonight.setup(opts)
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = 'tokyonight',
+  group = vim.api.nvim_create_augroup('tokyonight_custom_highlight', { clear = true }),
+  callback = function()
+    local set_hl = vim.api.nvim_set_hl
+
+    -- HACK: Fix colors for `copilot.vim` with Comment highlight
+    local comment_hl = vim.api.nvim_get_hl(0, { name = 'Comment' })
+    set_hl(0, 'CopilotSuggestion', { fg = comment_hl.fg })
+
+    -- HACK: Fix colors for lua/plugins/custom/statuscolumn.lua with CursorLineNr highlight
+    local curlineNr_hl = vim.api.nvim_get_hl(0, { name = 'CursorLineNr' })
+    set_hl(0, 'StatusColumnMark', { fg = curlineNr_hl.fg })
+  end,
+})
+
+vim.cmd.colorscheme('tokyonight')
