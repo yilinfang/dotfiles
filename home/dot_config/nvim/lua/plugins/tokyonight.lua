8003111d525e05f6c934f2d25190269e4c320706
Yilin Fang <qzfyl98@outlook.com>
2025-10-18 13:18:17 -0400

    feat(nvim): I don't like italic variants of current font

--- home/dot_config/nvim/lua/plugins/tokyonight.lua
+++ home/dot_config/nvim/lua/plugins/tokyonight.lua
@@ -4,6 +4,12 @@
 local tokyonight = require('tokyonight')
 local opts = {
   style = 'night',
+  styles = {
+    comments = { italic = false },
+    keywords = { italic = false },
+    functions = { italic = false },
+    variables = { italic = false },
+  },
   plugins = {
     all = true, -- NOTE: Enable all plugins
     auto = false, -- NOTE: We are not using `lazy.nvim`
@@ -19,7 +25,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 
     -- HACK: Fix colors for `copilot.vim` with Comment highlight
     local comment_hl = vim.api.nvim_get_hl(0, { name = 'Comment' })
-    set_hl(0, 'CopilotSuggestion', { fg = comment_hl.fg })
+    set_hl(0, 'CopilotSuggestion', { fg = comment_hl.fg, italic = true })
 
     -- HACK: Fix colors for lua/plugins/custom/statuscolumn.lua with CursorLineNr highlight
     local curlineNr_hl = vim.api.nvim_get_hl(0, { name = 'CursorLineNr' })
