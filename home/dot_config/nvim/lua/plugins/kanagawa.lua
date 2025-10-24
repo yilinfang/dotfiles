6f38b456971d09a0f9cc542ee7ee5295baee0ff6
Yilin Fang <qzfyl98@outlook.com>
2025-10-23 20:59:57 -0400

    feat(*): Theme kanagawa-wave

--- home/dot_config/nvim/lua/plugins/kanagawa.lua
+++ home/dot_config/nvim/lua/plugins/kanagawa.lua
@@ -17,9 +17,12 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   callback = function()
     local set_hl = vim.api.nvim_set_hl
 
-    -- HACK: Fix colors for `copilot.vim` with Comment highlight
-    local comment_hl = vim.api.nvim_get_hl(0, { name = 'Comment' })
-    set_hl(0, 'CopilotSuggestion', { fg = comment_hl.fg })
+    -- -- HACK: Fix colors for `copilot.vim` with Comment highlight
+    -- local comment_hl = vim.api.nvim_get_hl(0, { name = 'Comment' })
+    -- set_hl(0, 'CopilotSuggestion', { fg = comment_hl.fg })
+
+    -- HACK: Use terminal color 8 (BrightBlack) for Copilot suggestions
+    set_hl(0, 'CopilotSuggestion', { fg = vim.g.terminal_color_8 })
 
     -- HACK: Fix colors for lua/plugins/custom/statuscolumn.lua with CursorLineNr highlight
     local curlineNr_hl = vim.api.nvim_get_hl(0, { name = 'CursorLineNr' })
