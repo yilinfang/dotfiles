661b9cdd5420814d62bf3f1a2b323615c79ac358
Yilin Fang <qzfyl98@outlook.com>
2025-10-24 22:52:20 -0400

    revert(nvim): Bring mini.indentscope back

--- home/dot_config/nvim/lua/plugins/mini-indentscope.lua
+++ home/dot_config/nvim/lua/plugins/mini-indentscope.lua
@@ -1,8 +1,8 @@
 -- lua/plugins/mini-indentscope.lua
 -- Configuration for `mini.indentscope`
 
--- HACK: Disable drawing indent guides since we only use the textobjects
-vim.g.miniindentscope_disable = true
+-- -- HACK: Disable drawing indent guides since we only use the textobjects
+-- vim.g.miniindentscope_disable = true
 
 -- -- HACK: Create an autocmd to disable indentscope in certain filetypes
 -- vim.api.nvim_create_autocmd("FileType", {
