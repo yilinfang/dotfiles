f4993e3a99d092a68049f6c081515f3149e5d281
Yilin Fang <qzfyl98@outlook.com>
2025-12-23 14:34:49 -0500

    style(nvim): Rewrite vimscript configurations with lua

---
+++ home/dot_config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,9 @@
+-- after/ftplugin/python.lua
+-- Custom settings for Python files
+
+-- Set 4 spaces for indentation in Python files
+-- Follow the PEP 8 style guide: https://peps.python.org/pep-0008/#indentation
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
