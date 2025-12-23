1ac92d6378d0eaed6d6d491a201f1bd06ef888d5
Yilin Fang <qzfyl98@outlook.com>
2025-12-23 18:23:47 -0500

    revert(nvim): Revert "feat(nvim): Don't show the scope from mini.indentscope"
    This reverts commit 934e10e79ba480a19b6a471963c03b578544babd.

--- home/dot_config/nvim/lua/config/mini-indentscope.lua
+++ home/dot_config/nvim/lua/config/mini-indentscope.lua
@@ -1,8 +1,8 @@
 -- lua/config/mini-indentscope.lua
 -- Configuration for `mini.indentscope`
 
--- HACK: Disable drawing indent guides since we only use the textobjects
-vim.g.miniindentscope_disable = true
+-- -- HACK: Disable drawing indent guides since we only use the textobjects
+-- vim.g.miniindentscope_disable = true
 
 -- -- HACK: Create an autocmd to disable indentscope in certain filetypes
 -- vim.api.nvim_create_autocmd('FileType', {
@@ -16,11 +16,10 @@ vim.g.miniindentscope_disable = true
 -- })
 
 local indentscope = require('mini.indentscope')
--- local opts = {
---   draw = {
---     animation = indentscope.gen_animation.none(),
---   },
---   options = { try_as_border = true },
--- }
-local opts = {}
+local opts = {
+  draw = {
+    animation = indentscope.gen_animation.none(),
+  },
+  -- options = { try_as_border = true },
+}
 indentscope.setup(opts)
