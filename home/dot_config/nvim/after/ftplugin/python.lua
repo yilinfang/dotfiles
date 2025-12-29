1204cbdfd8722046fe603318d92fb7dea766d115
Yilin Fang <qzfyl98@outlook.com>
2025-12-29 02:56:51 -0500

    fix(nvim): Use buffer-local options to keep the override scoped to Python files

--- home/dot_config/nvim/after/ftplugin/python.lua
+++ home/dot_config/nvim/after/ftplugin/python.lua
@@ -1,9 +1,8 @@
 -- after/ftplugin/python.lua
 -- Custom settings for Python files
 
--- Set 4 spaces for indentation in Python files
 -- Follow the PEP 8 style guide: https://peps.python.org/pep-0008/#indentation
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true
